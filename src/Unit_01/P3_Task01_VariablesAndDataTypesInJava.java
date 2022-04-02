913efc1547b85b80e09883c1ed6027f0c27c8701
unknown <7neerajrawat@gmail.com>
2022-04-02 20:58:51 +0530

    first commit

---
+++ src/Unit_01/P3_Task01_VariablesAndDataTypesInJava.java
@@ -0,0 +1,16 @@
+package Unit_01;
+
+public class P3_Task01_VariablesAndDataTypesInJava {
+	  static int a=11;
+	  int b=10;
+	  
+	  public static void main(String[] args)
+	  {
+		  int a,b,c;
+		  float pi;
+		  double d;
+		  char e;
+		  
+		  System.out.println(b);
+	  }
+}
